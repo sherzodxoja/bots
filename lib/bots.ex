e6d3a11255578878ba6814fa84223114c10571b2
Mikhail Yashkov <mike25@ya.ru>
2016-07-09 21:03:44 +0300

    don't start web server when empty bots spec

--- lib/bots.ex
+++ lib/bots.ex
@@ -9,9 +9,9 @@ defmodule Bots do
 		case Application.get_env(:bots, :bots_spec) do
 			list_of_bots when is_list(list_of_bots) and length(list_of_bots) > 0 ->
 				Bots.BotsSupervisor.start_link list_of_bots
+				Bots.WebSupervisor.start_link
 			_->
 				Logger.info "No bots specifications to start"
 		end
-		Bots.WebSupervisor.start_link
 	end
 end
