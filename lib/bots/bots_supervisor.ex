abfcea532a336b655ea52248f87ea798cc245ffe
Mikhail Yashkov <mike25@ya.ru>
2016-07-11 23:27:25 +0300

    tests

--- lib/bots/bots_supervisor.ex
+++ lib/bots/bots_supervisor.ex
@@ -3,6 +3,7 @@ defmodule Bots.BotsSupervisor do
 	This supervisor for each bots in the list of specifications creates process according to bot's type. If type is `:active` will be created process with long-polling implementation. If type is `:passive` will becreated process with webhook implementation.
 	"""
 	use Supervisor
+	require Logger
 
 	@supervisor_name MainBotsSupervisor
 
@@ -12,33 +13,96 @@ defmodule Bots.BotsSupervisor do
 
 	def init(bots_specs) do
 		# Creating workers from bots specifications
-		# TODO:
-		#	1. adding bots 'on-the-fly'
 		children = Enum.map(bots_specs, fn(bot)->
-			{name, type, options} = bot
-			module = case type do
-				:active->
-					Bots.Telegram.BotActive;
-				:passive->
-					Bots.Telegram.BotPassive;
+			case prepare_worker_spec(bot) do
+				{:ok, spec}->
+					spec
+				_->
+					[]
 			end
-			worker(module, [options], [id: name])
 		end)
-		supervise(children, strategy: :one_for_one)
+		supervise(List.flatten(children), strategy: :one_for_one)
+	end
+
+	def create_bot(bot_spec) do
+		case prepare_worker_spec(bot_spec) do
+			{:ok, spec} ->
+				case Supervisor.start_child(@supervisor_name, spec) do
+					{:ok, _}->
+						:ok
+					{:ok, _, _}->
+						:ok
+					{:error, {:already_started, _}}->
+						:already_started
+					{:error, already_present}->
+						:already_present
+					e->
+						IO.puts inspect e
+						e
+				end
+			Error->
+				Error
+		end
 	end
 
 	@doc """
-	Searching bot's process by name and puts into it message
+	Валидация и подготовка спецификации бота
+
+	## Примеры
+		iex> Bots.BotsSupervisor.prepare_worker_spec({"bot_name", :active, []})
+		{:ok, {"bot_name", {Bots.Telegram.BotActive, :start_link, [[]]}, :permanent, 5000, :worker, [Bots.Telegram.BotActive]}}
+
+		iex> Bots.BotsSupervisor.prepare_worker_spec({"bot_name", :active, [], 1})
+		:bad_bot_spec
+
+		iex> Bots.BotsSupervisor.prepare_worker_spec({"bot_name", :medium, []})
+		:bad_bot_type
+	"""
+	def prepare_worker_spec(bot_spec) do
+		try do
+			case bot_spec do
+				_ when is_tuple(bot_spec) and tuple_size(bot_spec) == 3 ->
+					{name, type, options} = bot_spec
+					module = case type do
+						:active->
+							Bots.Telegram.BotActive
+						:passive->
+							Bots.Telegram.BotPassive
+						_->
+							throw :bad_bot_type
+					end
+					{:ok, worker(module, [options], [id: name])}
+				_->
+					throw :bad_bot_spec
+			end
+		catch
+			error->
+				Logger.error "Error: #{inspect error} in spec #{inspect bot_spec}"
+				error
+		end
+	end
+
+	@doc """
+	Поиск процесса бота по имени и отправка сообщения в этот процесс
+
+		iex> Bots.BotsSupervisor.send_msg_to_worker("bot_name_that_does_not_exists", "msg")
+		:bot_not_found
+
+		iex> Bots.BotsSupervisor.create_bot({"bot_name_that_exists", :active, []})
+		iex> Bots.BotsSupervisor.send_msg_to_worker("bot_name_that_exists", "msg")
+		:ok
 	"""
 	def send_msg_to_worker(name, msg) do
 		children = Supervisor.which_children(@supervisor_name)
+		IO.puts inspect children
 		case :lists.keyfind(name, 1, children) do
-			false->
-				IO.puts "unknown bot " <> inspect name
-			{_, pid, _, _} ->
+			{_, pid, _, _}->
 				Bots.Telegram.BotPassive.new_message(pid, msg)
-			B->
-				IO.puts inspect B
+				:ok
+			undefined->
+				Logger.error "Bot not found: #{inspect name}"
+				:bot_not_found
 		end
 	end
+
 end
---
+++ test/bots_test.exs
@@ -0,0 +1,17 @@
+defmodule BotAppTest do
+  use ExUnit.Case
+  doctest Bots.BotsSupervisor
+
+  test "bot specifications" do
+    assert Bots.BotsSupervisor.prepare_worker_spec({"bot_name", :active, [], 1}) == :bad_bot_spec
+    assert Bots.BotsSupervisor.prepare_worker_spec({"bot_name", :medium, []}) == :bad_bot_type
+    assert Bots.BotsSupervisor.prepare_worker_spec({"bot_name", :active, []}) == {:ok, {"bot_name", {Bots.Telegram.BotActive, :start_link, [[]]}, :permanent, 5000, :worker, [Bots.Telegram.BotActive]}}
+    assert Bots.BotsSupervisor.prepare_worker_spec({"bot_name", :passive, []}) == {:ok, {"bot_name", {Bots.Telegram.BotPassive, :start_link, [[]]}, :permanent, 5000, :worker, [Bots.Telegram.BotPassive]}}
+  end
+
+  test "bots duplication" do
+  	assert Bots.BotsSupervisor.create_bot({"bot_name1", :active, []}) == :ok
+  	assert Bots.BotsSupervisor.create_bot({"bot_name2", :active, []}) == :ok
+  	assert Bots.BotsSupervisor.create_bot({"bot_name1", :passive, []}) == :already_started
+  end
+end
