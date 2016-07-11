585165d63a51065daf82198334dab59a61175c92
Mikhail Yashkov <mike25@ya.ru>
2016-07-11 23:29:29 +0300

    filling up documentation

--- lib/bots/telegram/processor.ex
+++ lib/bots/telegram/processor.ex
@@ -1,8 +1,18 @@
 defmodule Bots.Telegram.Processor do
+	@moduledoc """
+	Декодирует json-сообщения, делегирует готовые объекты нужному коммандеру и отправляет ответ на сервер Телеграма.
+
+	Модуль Processor декодирует приходящее в виде json сообщение от Телеграма в структуру `Bots.Telegram.TelegramResponse` в соответствии с официальной документацией Bot API. После парсинга структура передаётся в отдельный поток, где передаётся в коммандер, заданный в опциях бота. После получения ответа от коммандера происходит отправка подготовленных данных на сервере Телеграма.
+	"""
 	import Bots.Telegram.TelegramResponse
 	require Logger
 
 	
+	@doc """
+	Декодирование json-данных, пришедших методом webhook
+
+	Данные декодируются в структуру `Bots.Telegram.TelegramResponse`. После успешного декодирования результат отправляется в отдельный поток с функцией `process_messages`
+	"""
 	def decode_single_update(data, options) do
 		{:ok, decoded} = Poison.decode(data, as: 
 			%Bots.Telegram.TelegramResponse.Update {
@@ -15,6 +25,11 @@ defmodule Bots.Telegram.Processor do
 		spawn(__MODULE__, :process_messages, [[decoded], options])
 	end	
 
+	@doc """
+	Декодирование json-данных, пришедших в ответ на запрос
+
+	Данные декодируются в структуру `Bots.Telegram.TelegramResponse`. После успешного декодирования результат отправляется в отдельный поток с функцией `process_messages`. Более того, этот метод возвращает максимальный идентификатор объекта update среди пришедших.
+	"""
 	def decode(data, options) do
 		{:ok, decoded} = Poison.decode(data, as: 
 			%Bots.Telegram.TelegramResponse.Response {
@@ -47,6 +62,11 @@ defmodule Bots.Telegram.Processor do
 		end
 	end
 
+	@doc """
+	Параллельная обработка сообщений
+
+	В опциях должен быть представлен коммандер для обработки сообщения и токен для отправки ответа. Каждое сообщение из массива выполняется в своём потоке.
+	"""
 	def process_messages(data, options) do
 		commander = options[:commander]
 		token = options[:token]
@@ -55,6 +75,9 @@ defmodule Bots.Telegram.Processor do
 		end)
 	end
 
+	@doc """
+	Получение ответа от коммандера и передача его в метод отправки
+	"""
 	def prepare_and_send(commander, update, token) do
 		reply_msg = commander.get_response(update)
 		send_message(token, reply_msg, update.message.chat.id)
